c8f3484a2a2caa018635c99ad0ce49558905d2b9
abdulaziz2501 <abduhakimovabdulaziz52@gmail.com>
2025-01-06 16:30:07 +0500

    Django

--- book/urls.py
+++ book/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from views import my_books
+from .views import my_books, back
 
 
 urlpatterns=[
-    path('', my_books, namme='books')
+    path('', my_books, name='books'),
+    path('back/', back, name='ortga')
 ]
--- book/views.py
+++ book/views.py
@@ -1,10 +1,17 @@
 from django.http import HttpResponse
 
-
 def my_books(request):
+    html = """
+        <p> my books </p>
+        <a href="back/"> books</a>
+
+        """
+    return HttpResponse(html)
+
+def back(request):
     html="""
-    <p> my books </p>
-    <a href="book/"> books</a>
+    <p> kitoblar </p>
+    <a href="../"> ortga</a>
     
     """
 
