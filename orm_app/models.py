88593ed821e65e5ee49c2a917746a76ded27eeac
abdulaziz2501 <abduhakimovabdulaziz52@gmail.com>
2025-01-08 22:49:18 +0500

    inspectdb

--- orm_app/models.py
+++ orm_app/models.py
@@ -8,201 +8,83 @@
 from django.db import models
 
 
-class AuthGroup(models.Model):
-    name = models.CharField(unique=True, max_length=150)
+class Countries(models.Model):
+    country_id = models.TextField(primary_key=True)
+    country_name = models.TextField()
+    region = models.ForeignKey('Regions', models.DO_NOTHING)
 
     class Meta:
         managed = False
-        db_table = 'auth_group'
+        db_table = 'countries'
 
 
-class AuthGroupPermissions(models.Model):
-    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
-    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_group_permissions'
-        unique_together = (('group', 'permission'),)
-
-
-class AuthPermission(models.Model):
-    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
-    codename = models.CharField(max_length=100)
-    name = models.CharField(max_length=255)
+class Departments(models.Model):
+    department_id = models.AutoField(primary_key=True)
+    department_name = models.TextField()
+    location = models.ForeignKey('Locations', models.DO_NOTHING)
 
     class Meta:
         managed = False
-        db_table = 'auth_permission'
-        unique_together = (('content_type', 'codename'),)
+        db_table = 'departments'
 
 
-class AuthUser(models.Model):
-    password = models.CharField(max_length=128)
-    last_login = models.DateTimeField(blank=True, null=True)
-    is_superuser = models.BooleanField()
-    username = models.CharField(unique=True, max_length=150)
-    last_name = models.CharField(max_length=150)
-    email = models.CharField(max_length=254)
-    is_staff = models.BooleanField()
-    is_active = models.BooleanField()
-    date_joined = models.DateTimeField()
-    first_name = models.CharField(max_length=150)
+class Dependents(models.Model):
+    dependent_id = models.AutoField(primary_key=True)
+    first_name = models.TextField()
+    last_name = models.TextField()
+    relationship = models.TextField()
+    employee = models.ForeignKey('Employees', models.DO_NOTHING)
 
     class Meta:
         managed = False
-        db_table = 'auth_user'
+        db_table = 'dependents'
 
 
-class AuthUserGroups(models.Model):
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user_groups'
-        unique_together = (('user', 'group'),)
-
-
-class AuthUserUserPermissions(models.Model):
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+class Employees(models.Model):
+    employee_id = models.AutoField(primary_key=True)
+    first_name = models.TextField(blank=True, null=True)
+    last_name = models.TextField()
+    email = models.TextField()
+    phone_number = models.TextField(blank=True, null=True)
+    hire_date = models.TextField()
+    job = models.ForeignKey('Jobs', models.DO_NOTHING)
+    salary = models.TextField()  # This field type is a guess.
+    manager = models.ForeignKey('self', models.DO_NOTHING, blank=True, null=True)
+    department = models.ForeignKey(Departments, models.DO_NOTHING)
 
     class Meta:
         managed = False
-        db_table = 'auth_user_user_permissions'
-        unique_together = (('user', 'permission'),)
+        db_table = 'employees'
 
 
-class BlogAuthor(models.Model):
-    name = models.CharField(max_length=100, blank=True, null=True)
-    age = models.SmallIntegerField()
-
-    class Meta:
-        managed = False
-        db_table = 'blog_author'
-
-
-class BlogBook(models.Model):
-    name = models.CharField(max_length=100)
-    author = models.ForeignKey(BlogAuthor, models.DO_NOTHING, blank=True, null=True)
-    genre = models.ForeignKey('BlogGenre', models.DO_NOTHING, blank=True, null=True)
+class Jobs(models.Model):
+    job_id = models.AutoField(primary_key=True)
+    job_title = models.TextField()
+    min_salary = models.TextField()  # This field type is a guess.
+    max_salary = models.TextField()  # This field type is a guess.
 
     class Meta:
         managed = False
-        db_table = 'blog_book'
+        db_table = 'jobs'
 
 
-class BlogGenre(models.Model):
-    name = models.CharField(max_length=100)
-
-    class Meta:
-        managed = False
-        db_table = 'blog_genre'
-
-
-class BlogPost(models.Model):
-    title = models.CharField(max_length=250)
-    content = models.TextField()
-    publish = models.DateTimeField()
-    created = models.DateTimeField()
-    updated = models.DateTimeField()
-    author = models.ForeignKey(BlogAuthor, models.DO_NOTHING, blank=True, null=True)
+class Locations(models.Model):
+    location_id = models.AutoField(primary_key=True)
+    street_address = models.TextField(blank=True, null=True)
+    postal_code = models.TextField(blank=True, null=True)
+    city = models.TextField()
+    state_province = models.TextField(blank=True, null=True)
+    country = models.ForeignKey(Countries, models.DO_NOTHING)
 
     class Meta:
         managed = False
-        db_table = 'blog_post'
+        db_table = 'locations'
 
 
-class DjangoAdminLog(models.Model):
-    object_id = models.TextField(blank=True, null=True)
-    object_repr = models.CharField(max_length=200)
-    action_flag = models.PositiveSmallIntegerField()
-    change_message = models.TextField()
-    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-    action_time = models.DateTimeField()
+class Regions(models.Model):
+    region_id = models.AutoField(primary_key=True)
+    region_name = models.TextField()
 
     class Meta:
         managed = False
-        db_table = 'django_admin_log'
-
-
-class DjangoContentType(models.Model):
-    app_label = models.CharField(max_length=100)
-    model = models.CharField(max_length=100)
-
-    class Meta:
-        managed = False
-        db_table = 'django_content_type'
-        unique_together = (('app_label', 'model'),)
-
-
-class DjangoMigrations(models.Model):
-    app = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
-    applied = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'django_migrations'
-
-
-class DjangoSession(models.Model):
-    session_key = models.CharField(primary_key=True, max_length=40)
-    session_data = models.TextField()
-    expire_date = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'django_session'
-
-
-class StudentsAppFakultet(models.Model):
-    nomi = models.CharField(unique=True, max_length=150)
-    dekan = models.CharField(max_length=100, blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'students_app_fakultet'
-
-
-class StudentsAppGuruh(models.Model):
-    nomi = models.CharField(max_length=50)
-    boshlanish_yili = models.PositiveIntegerField()
-    bitiruv_yili = models.PositiveIntegerField()
-    yonalish = models.ForeignKey('StudentsAppYonalish', models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'students_app_guruh'
-
-
-class StudentsAppOqituvchi(models.Model):
-    toliq_ismi = models.CharField(max_length=150)
-    mutaxassislik = models.CharField(max_length=150)
-    fakultet = models.ForeignKey(StudentsAppFakultet, models.DO_NOTHING, blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'students_app_oqituvchi'
-
-
-class StudentsAppTalaba(models.Model):
-    ismi = models.CharField(max_length=100)
-    familiyasi = models.CharField(max_length=100)
-    tugilgan_sana = models.DateField()
-    guruh = models.ForeignKey(StudentsAppGuruh, models.DO_NOTHING, blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'students_app_talaba'
-
-
-class StudentsAppYonalish(models.Model):
-    nomi = models.CharField(max_length=150)
-    fakultet = models.ForeignKey(StudentsAppFakultet, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'students_app_yonalish'
+        db_table = 'regions'
