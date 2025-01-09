4fb468269f8a1b819237f8864ae599bf82547d5d
abdulaziz2501 <abduhakimovabdulaziz52@gmail.com>
2025-01-10 01:56:10 +0500

    static

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def blog_lists(request):
-    html="""
-    <p>blog</p>"""
-    return HttpResponse(html)
+    posts= Post.objects.all()
+    post_list=""
+    for post in posts:
+        post_list=f"<li>{post}</li>"
+    return HttpResponse(f"<ul>{post_list}</ul>")
--- orm_app/views.py
+++ orm_app/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from .models import Countries
+from django.http import HttpResponse
 
 # Create your views here.
 def orm_list(request):
     countries= Countries.objects.all()
+    # for c in countries:
+    #     print(c)
+    country_list =""
     for c in countries:
-        print(c)
+        country_list +=f"<li>{c}</li>"
+    return HttpResponse(f"<ul>{country_list}</ul>")
+
 
