eacb54a9cd1aaa01d01fb6736ae59c67a9564796
abdulaziz2501 <abduhakimovabdulaziz52@gmail.com>
2025-01-17 18:03:48 +0500

    my site

--- my_site/demoapp/models.py
+++ my_site/demoapp/models.py
@@ -2,11 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Customer(models.Model):
-    first_name=models.CharField(max_length=100, blank=False, null=False)
-    last_name=models.CharField(max_length=100, blank=False, null=False)
-    phone_number=models.CharField(max_length=100, blank=False, null=False)
+    full_name=models.CharField(max_length=100, blank=False, null=False)
+    user_name=models.CharField(max_length=100, blank=False, null=False)
+    phone_number=models.CharField(max_length=11, blank=False, null=False)
     email=models.CharField(max_length=100, blank=False, null=False)
-    message=models.TextField(blank=False, null=False)
+    password=models.CharField(max_length=100, blank=False, null=False)
+    confirm_password=models.CharField(max_length=100, blank=False, null=False)
+
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
